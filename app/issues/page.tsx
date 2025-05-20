c6ca0ca00ef1192e79b659137b893751e260ecfd
john lester escarlan <jlescarlan11@gmail.com>
2025-05-20 20:53:22 +0800

    organize imoprt statements

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,8 @@
-import React from "react";
+import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
-import IssueActions from "./IssueActions";
 import { Table } from "@radix-ui/themes";
-import Link from "../components/Link";
-import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
+import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
