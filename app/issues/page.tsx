14ef5ab39a53c0bf6d582d9bdb9f5806ef341007
john lester escarlan <jlescarlan11@gmail.com>
2025-05-20 22:34:40 +0800

    delay

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,11 @@
 import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import delay from "delay";
+
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
   return (
     <div>
       <IssueActions />
