f0c996e2f06e4fb706929d7cc6ebfd7234183b24
john lester escarlan <jlescarlan11@gmail.com>
2025-05-20 21:19:25 +0800

    Apply single responsibility principles

--- app/issues/[id]/EditIssueButton.tsx
+++ app/issues/[id]/EditIssueButton.tsx
@@ -1,22 +1,17 @@
 "use client";
-import { Issue } from "@prisma/client";
 import { Pencil2Icon } from "@radix-ui/react-icons";
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
 
-interface Props {
-  issue: Issue;
-}
-
-const IssueDetailActions = ({ issue }: Props) => {
+const EditIssueButton = ({ issueId }: { issueId: string }) => {
   return (
     <div>
       <Button>
         <Pencil2Icon />
-        <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
       </Button>
     </div>
   );
 };
 
-export default IssueDetailActions;
+export default EditIssueButton;
